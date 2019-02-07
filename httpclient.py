218814d5ae3f0bc5641644de67a4fce92b844cbd
Toluwanimi I. Akindele <tiakindele11@gmail.com>
2019-02-06 23:53:39 -0700

    license added to client

--- httpclient.py
+++ httpclient.py
@@ -18,6 +18,22 @@
 # Write your own HTTP GET and POST
 # The point is to understand what you have to send and get experience with it
 
+"""
+Copyright 2019 Toluwanimi Akindele
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import sys
 import socket
 import re
@@ -67,10 +83,7 @@ class HTTPClient(object):
                 buffer.extend(part)
             else:
                 done = not part
-        # try:
         return buffer.decode('utf-8', errors='ignore')
-        # except UnicodeDecodeError:
-        #     print ("UnicodeDecodeError at part: {}".format(part))
 
     def GET(self, url, args=None):
         urlP = urllib.parse.urlparse(url)
